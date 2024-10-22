4914a65ef1519e39552f6a4f57f17076b45dc242
Wiwass <140823790+Wiwass@users.noreply.github.com>
2024-10-22 17:42:32 +0200

    Finito

--- Esercizio2.c
+++ Esercizio2.c
@@ -9,16 +9,16 @@ int main(int argc, char* argv[])
     if(argc==1)
         return 0;
     int x[argc-1];
-    for(int i=1;i<=argc;i++){
+    for(int i=0;i<argc-1;i++){
     	
-        x[i-1]=atoi(argv[i-1]); // uso atoi al posto di strol perchè non ha senso complicarsi la vita e non so esattamente come vengano passati sull'argv gli argomenti
+        x[i]=atoi(argv[i+1]); // uso atoi al posto di strol perchè non ha senso complicarsi la vita e non so esattamente come vengano passati sull'argv gli argomenti
         
-        if(x[i-1]>x[max_position]){
-            max_potition=i-1;
-        }
-        if(x[i-1]>x[min_position]){
-            min_potition=i-1;
-        }  
+        if ( x[i] > x[max_position] )
+            max_position=i;
+
+        if ( x[i] < x[min_position] )
+            min_position=i;
+ 
 	}
     
     int temp;
